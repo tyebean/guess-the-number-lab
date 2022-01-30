575135f6bbf3ee4878a3ceb2c00b44e830c4c3aa
tyebean <flower.webster@gmail.com>
2022-01-30 16:53:13 -0500

    work on #3

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+//psedocode #3
+        //  do... while 
+        // do - return a number
+        // use parseInt return a value that is a number
+        // and also between biggest and smallest nums
+        //let guess | placeholder
+        //return nan if prompt is NaN
+
 const game = {
   title: 'Guess the Number!',
   biggestNum: 100,
@@ -5,44 +13,38 @@ const game = {
   secretNum: null,
   prevGuesses: [],
   getGuess: function() {
-    // let parsed = parseInt(num)
-    // if (num.isNaN(parsed)) {console.log('That is not a number.')}
-    // while (num <= this.biggestNum && num >= this.smallestNum){
-    //   console.log()
-    // const guessPrompt = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`)
-    // return guessPrompt
-
-    //psedocode 
-    //  while 
-    // use parseInt return a value that is a number(not a string
-    //and also between biggest and smallest nums
-    //let guess | placeholder
-
-    
-
-
+    let guess;
+    do {
+      guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`))
+    }
+    while (
+      guess <= this.biggestNum || guess >= this.smallestNum);
+      return guess
+    }
     } 
-    
-  }, 
-  play: function() {
-    this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-      let theplayersGuess = ""
-      while(theplayersGuess !== this.getGuess){
-        this.prevGuesses.push([])
-        console.log(`Your guess was incorrect. Try again!`)
-      }
-  },
-  weRend: function(){
-    render()
-  if(theplayerGuess === this.getGuess){
-    console.log(`Congrats! You have guessed the number in ${this.prevGuesses.length} guesses!`)
-  } else {
-    console.log(`Your guess is too [high|low]. Your previous guesses are ${this.prevGuesses.join(', ')}.`)
-  }
-}
-}
-game.play()
+
+
+
+
+//   play: function() {
+//     this.secretNum = Math.floor(Math.random() * 
+//       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+//       let theplayersGuess = ""
+//       while(theplayersGuess !== this.getGuess){
+//         this.prevGuesses.push([])
+//         console.log(`Your guess was incorrect. Try again!`)
+//       }
+//   },
+//   weRend: function(){
+//     render()
+//   if(theplayerGuess === this.getGuess){
+//     console.log(`Congrats! You have guessed the number in ${this.prevGuesses.length} guesses!`)
+//   } else {
+//     console.log(`Your guess is too [high|low]. Your previous guesses are ${this.prevGuesses.join(', ')}.`)
+//   }
+// }
+
+// game.play()
 
 //high/low
 //console log frequently
@@ -51,7 +53,3 @@ game.play()
 //a prompt will return the vaule that tjhe user will type in
 //ptompt is equal to var so it can store what the user will put in
 // const user age = prompt("how old are you?")
-
-
-
-
