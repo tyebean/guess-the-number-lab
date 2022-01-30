dd9814b089caaeb5d56994f84c7267d0c0e95e28
tyebean <flower.webster@gmail.com>
2022-01-30 14:03:12 -0500

    play with render

--- app.js
+++ app.js
@@ -17,7 +17,14 @@ const game = {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
   }
-  if (i = 0; i <= getGuess; i++) {
-    prevGuesses.push 
-  }
+  let theplayersGuess = null
+  while(theplayersGuess !== getGuess){
+    this.prevGuesses.push([])
+  },
+  weRender: 
+  if(playerGuess === getGuess){
+    console.log(`Congrats! You have guessed the number in ${prevGuesses.length}` guesses!)
+  } else {
+    console.log(`Your guess is too [high|low]. Your previous guesses are ${prevGuesses.join(', ')}.`)
+  },
 }
