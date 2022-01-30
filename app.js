537264c13f00fe46bab1fa611c0b54c554e42ecd
tyebean <flower.webster@gmail.com>
2022-01-30 13:04:26 -0500

    finish #3: getGuess

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
+  getGuess: 
+  function(num) {
+    let parsed = parseInt(num)
+    while (num <= this.biggestNum && num >= this.smallestNum){
+      return this.getGuess
+    } 
+    if (num.isNAN(parsed)) {return console.log('That is not a number.')}
+    console.log(`Enter a guess between ${smallestNum} and ${biggestNum}.`)
+  },
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
   }
 }
-
