9e869622c09c6058393571a195ebb08808230283
tyebean <flower.webster@gmail.com>
2022-01-30 23:29:34 -0500

    add method weRend

--- app.js
+++ app.js
@@ -24,36 +24,46 @@ const game = {
     }
     while ( 
       isNaN(guess) || guess > this.biggestNum || guess < this.smallestNum);
-      console.log(game.getGuess())
+      // console.log(game.getGuess())
     },
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
       do {
-        this.prevGuesses.push(this.getGuess)
-          console.log(this.prevGuesses)
-      } while (
-        this.getGuess !== this.secretNum
-      )
-      console.log(`The number ${this.getGuess} isn't quite right. Try again!`)
-    } 
-} 
-
-//   },
-//   weRend: function(){
-//     render()
-//   if(theplayerGuess === this.getGuess){
-//     console.log(`Congrats! You have guessed the number in ${this.prevGuesses.length} guesses!`)
-//   } else {
-//     console.log(`Your guess is too [high|low]. Your previous guesses are ${this.prevGuesses.join(', ')}.`)
-//   }
-// }
+        this.prevGuesses.push(this.getGuess())
+        //looked at solution and saw i was missing () at the end of this.getGuess() :(
+          // console.log(this.prevGuesses)
+      } //while (this.getGuess !== this.secretNum
+        while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum) 
+      // console.log(`The number ${this.getGuess} isn't quite right. Try again!`)
+    },
+  wrongOrRight: function(){
+  if(this.secretNum === this.getGuess){
+    console.log(`Congrats! You have guessed the number in ${this.prevGuesses.length} guesses!`)
+  } else {
+    console.log(`Your guess is too [high|low]. Your previous guesses are ${this.prevGuesses.join(', ')}.`)
+    }
+  },
+  weRend: function init(){
+    //set variables back to starting point
+    this.game.biggestNum = 100
+    this.game.smallestNum = 1
+    this.game.secretNum = null
+    this.game.prevGuesses = []
+  }
+    function acceptInput(){
+      //change variables according to user input 
+      render()
+    }
+      function render(){
+        //display the origional state of the variables to the user in a meaningful way
+      }
+  }
 
 game.play()
 
 //high/low
 //console log frequently
-//prompt the user to enter something (alert)
 //render()
 //a prompt will return the vaule that tjhe user will type in
 //ptompt is equal to var so it can store what the user will put in
