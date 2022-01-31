f44172ca2d99069ae09746dad9777b0419f76709
tyebean <flower.webster@gmail.com>
2022-01-31 10:34:25 -0500

    tweak render

--- app.js
+++ app.js
@@ -11,6 +11,7 @@
       // do (push getGuess into prevGuesses)
       //while getGuess !-- (is not equal to) secretNum
       // invoke game.getGuess() by saying something like: console.log(`${game.getGuess} was not the correct number. guess again!')
+
 const game = {
   title: 'Guess the Number!',
   biggestNum: 100,
@@ -23,48 +24,35 @@ const game = {
       guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`))
     }
     while ( 
-      isNaN(guess) || guess > this.biggestNum || guess < this.smallestNum);
-      // console.log(game.getGuess())
+      isNaN(guess) || guess <= this.biggestNum || guess >= this.smallestNum);
     },
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
       do {
         this.prevGuesses.push(this.getGuess())
-        //looked at solution and saw i was missing () at the end of this.getGuess() :(
-          // console.log(this.prevGuesses)
-      } //while (this.getGuess !== this.secretNum
-        while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum) 
-      // console.log(`The number ${this.getGuess} isn't quite right. Try again!`)
+        this.render()
+      } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum)
+      //get explanation about the above while loop?
     },
-  wrongOrRight: function(){
-  if(this.secretNum === this.getGuess){
-    console.log(`Congrats! You have guessed the number in ${this.prevGuesses.length} guesses!`)
-  } else {
-    console.log(`Your guess is too [high|low]. Your previous guesses are ${this.prevGuesses.join(', ')}.`)
-    }
-  },
-  weRend: function init(){
-    //set variables back to starting point
-    this.game.biggestNum = 100
-    this.game.smallestNum = 1
-    this.game.secretNum = null
-    this.game.prevGuesses = []
-  }
-    function acceptInput(){
-      //change variables according to user input 
-      render()
-    }
-      function render(){
-        //display the origional state of the variables to the user in a meaningful way
+    highOrLow: function(){
+      if (this.getGuess > this.secretNum) {
+        return "high"
+      } else if (this.getGuess < this.secretNum) {
+        return "low"
       }
+    },
+  render: function(){
+    if(this.secretNum === this.getGuess){
+      alert(`Congrats! You have guessed the number in ${this.prevGuesses.length} guesses!`)
+    } else {
+      alert(`Your guess is too ${this.highOrLow}. Your previous guesses are ${this.prevGuesses.join(", ")}.`)
+    }
   }
+}
 
 game.play()
 
-//high/low
-//console log frequently
+//still need to
+//add high/low
 //render()
-//a prompt will return the vaule that tjhe user will type in
-//ptompt is equal to var so it can store what the user will put in
-// const user age = prompt("how old are you?")
